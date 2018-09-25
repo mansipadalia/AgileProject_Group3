6d90c8f90566029486168cb2360b3b12d9492e96
Rozy Gupta <rozygupta024@gmail.com>
2018-09-25 16:37:19 -0400

    Created Classes

--- src/GEDCOM/Family.java
+++ src/GEDCOM/Family.java
@@ -1,5 +1,72 @@
 package GEDCOM;
 
+import java.util.GregorianCalendar;
+
 public class Family {
+
+    private String id;
+    private GregorianCalendar married;
+    private GregorianCalendar divorced;
+    private String husbandID;
+    private String husbandName;
+    private String wifeId;
+    private String wifeName;
+    private String children;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public GregorianCalendar getMarried() {
+        return married;
+    }
+    public void setMarried(GregorianCalendar married) {
+        this.married = married;
+    }
+
+    public GregorianCalendar getDivorced() {
+        return divorced;
+    }
+    public void setDivorced(GregorianCalendar divorced) {
+        this.divorced = divorced;
+    }
+
+    public String getHusbandID() {
+        return husbandID;
+    }
+    public void setHusbandID(String husbandID) {
+        this.husbandID = husbandID;
+    }
+
+    public String getHusbandName() {
+        return husbandName;
+    }
+    public void setHusbandName(String husbandName) {
+        this.husbandName = husbandName;
+    }
+
+    public String getWifeId() {
+        return wifeId;
+    }
+    public void setWifeId(String wifeId) {
+        this.wifeId = wifeId;
+    }
+
+    public String getWifeName() {
+        return wifeName;
+    }
+    public void setWifeName(String wifeName) {
+        this.wifeName = wifeName;
+    }
+
+    public String getChildren() {
+        return children;
+    }
+    public void setChildren(String children) {
+        this.children = children;
+    }
 
 }
--- src/GEDCOM/Individual.java
+++ src/GEDCOM/Individual.java
@@ -1,5 +1,82 @@
 package GEDCOM;
 
+import java.util.GregorianCalendar;
+
 public class Individual {
+        private String id;
+        private String name;
+        private char gender;
+        private GregorianCalendar birthday;
+        private int age;
+        private boolean alive;
+        private GregorianCalendar death;
+        private String child;
+        private String spouse;
+
+        public String getId() {
+            return id;
+        }
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public char getGender() {
+            return gender;
+        }
+        public void setGender(char gender) {
+            this.gender = gender;
+        }
+
+        public GregorianCalendar getBirthday() {
+            return birthday;
+        }
+        public void setBirthday(GregorianCalendar birthday) {
+            this.birthday = birthday;
+        }
+
+        public int getAge() {
+            return age;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public boolean isAlive() {
+            return alive;
+        }
+        public void setAlive(boolean alive) {
+            this.alive = alive;
+        }
+
+        public GregorianCalendar getDeath() {
+            return death;
+        }
+        public void setDeath(GregorianCalendar death) {
+            this.death = death;
+        }
+
+        public String getChild() {
+            return child;
+        }
+        public void setChild(String child) {
+            this.child = child;
+        }
+
+        public String getSpouse() {
+            return spouse;
+        }
+        public void setSpouse(String spouse) {
+            this.spouse = spouse;
+        }
+    }
+
+
+
 	
-}
