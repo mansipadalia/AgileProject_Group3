3e756fb37a71ddbb3f2dd28a41ebf425b6b7639e
Mansi Padalia <mansi25padalia@gmail.com>
2018-09-26 00:09:02 -0400

    collection of Individuals and refactoring

--- src/GEDCOM/Individual.java
+++ src/GEDCOM/Individual.java
@@ -1,15 +1,17 @@
 package GEDCOM;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.GregorianCalendar;
 
 public class Individual {
         private String id;
         private String name;
-        private char gender;
-        private GregorianCalendar birthday;
+        private String gender;
+        private LocalDate birthday;
         private int age;
         private boolean alive;
-        private GregorianCalendar death;
+        private LocalDate death;
         private String child;
         private String spouse;
 
@@ -27,38 +29,39 @@ public class Individual {
             this.name = name;
         }
 
-        public char getGender() {
+        public String getGender() {
             return gender;
         }
-        public void setGender(char gender) {
+        public void setGender(String gender) {
             this.gender = gender;
         }
 
-        public GregorianCalendar getBirthday() {
+        public LocalDate getBirthday() {
             return birthday;
         }
-        public void setBirthday(GregorianCalendar birthday) {
+        public void setBirthday(LocalDate birthday) {
             this.birthday = birthday;
         }
 
         public int getAge() {
-            return age;
+        	LocalDate endDate = this.death !=null ? this.death : LocalDate.now();
+        	return Period.between(birthday, endDate).getYears();
         }
         public void setAge(int age) {
             this.age = age;
         }
 
         public boolean isAlive() {
-            return alive;
+        	return this.death == null;
         }
         public void setAlive(boolean alive) {
             this.alive = alive;
         }
 
-        public GregorianCalendar getDeath() {
+        public LocalDate getDeath() {
             return death;
         }
-        public void setDeath(GregorianCalendar death) {
+        public void setDeath(LocalDate death) {
             this.death = death;
         }
 
--- src/GEDCOM/Parser.java
+++ src/GEDCOM/Parser.java
@@ -3,12 +3,16 @@ package GEDCOM;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Parser {
-	
+
 	private List<Individual> IndividualList;
 	private List<Family> FamilyList;
 
@@ -19,7 +23,7 @@ public class Parser {
 	public void setIndividualList(List<Individual> individualList) {
 		IndividualList = individualList;
 	}
-	
+
 	public List<Family> getFamilyList() {
 		return FamilyList;
 	}
@@ -27,63 +31,106 @@ public class Parser {
 	public void setFamilyList(List<Family> familyList) {
 		FamilyList = familyList;
 	}
-	
-	public Parser() 
-	{
-	File file = new File("resources/InputGEDCOM.ged");
-	try {
-		BufferedReader br = new BufferedReader(new FileReader(file));
 
-		String st;
-		Set<String> tagNames = init();
-		int level;
-		String tag;
-		String arguments;
-		String tagValidity;
-		
-		while ((st = br.readLine()) != null)
-		{
-			String[] splited = st.split("\\s+",3);
-			if(tagNames.contains(splited[1]))
-			{
-				tagValidity="Y";
+	public Parser() {
+		File file = new File("resources/InputGEDCOM.ged");
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+
+			String st;
+			Set<String> tagNames = init();
+			String tagValidity;
+
+			List<Individual> IList = new ArrayList<Individual>();
+			Individual indi = new Individual();
+
+			boolean birth = false;
+			boolean death = false;
+			String month[] = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+
+			while ((st = br.readLine()) != null) {
+				String[] splited = st.split("\\s+", 3);
+
+				if (tagNames.contains(splited[1]))
+					tagValidity = "Y";
+				else if ((splited[2].equals("FAM")) || (splited[2].equals("INDI"))) {
+					String temp;
+					temp = splited[2];
+					splited[2] = splited[1];
+					splited[1] = temp;
+					tagValidity = "Y";
+				} else
+					tagValidity = "N";
+				if ((splited[1].equals("DATE")) && (splited[0].equals("1")))
+					tagValidity = "N";
+				if ((splited[1].equals("NAME")) && (splited[0].equals("2")))
+					tagValidity = "N";
+
+				if (tagValidity.equals("Y")) {
+					if (splited[1].equals("INDI") || splited[1].equals("FAM")) {
+						if (indi.getId() != null) {
+							IList.add(indi);
+							indi = new Individual();
+						}
+						if (splited[1].equals("INDI")) {
+							indi.setId(splited[2]);
+						}
+					}
+					if (splited[1].equals("NAME")) {
+						indi.setName(splited[2]);
+					}
+					if (splited[1].equals("SEX")) {
+						indi.setGender(splited[2]);
+					}
+					if (splited[1].equals("BIRT"))
+						birth = true;
+					if (splited[1].equals("DEAT"))
+						death = true;
+					if (splited[1].equals("DATE") && (birth == true || death == true)) {
+						String[] dateSplit = splited[2].split(" ");
+						if (birth == true) {
+							indi.setBirthday(LocalDate.of(Integer.parseInt(dateSplit[2]),
+									Arrays.asList(month).indexOf(dateSplit[1]) + 1, Integer.parseInt(dateSplit[0])));
+							birth = false;
+						}
+						if (death == true) {
+							indi.setDeath(LocalDate.of(Integer.parseInt(dateSplit[2]),
+									Arrays.asList(month).indexOf(dateSplit[1]) + 1, Integer.parseInt(dateSplit[0])));
+							death = false;
+						}
+					}
+					if (splited[1].equals("FAMC"))
+						indi.setChild(splited[2]);
+					if (splited[1].equals("FAMS"))
+						indi.setSpouse(splited[2]);
+				}
 			}
-			else if ((splited[2].equals("FAM")) || (splited[2].equals("INDI")))
-			{
-				String temp;
-				temp = splited[2];
-				splited[2] = splited[1];
-				splited[1] = temp;
-				tagValidity="Y";
+
+			for (Individual i : IList) {
+				System.out.println(i.getId());
+				System.out.println(i.getName());
+				System.out.println(i.getGender());
+				System.out.println(i.getBirthday());
+				System.out.println(i.getAge());
+				System.out.println(i.isAlive());
+				System.out.println(i.getDeath());
+				System.out.println(i.getChild());
+				System.out.println(i.getSpouse());
 			}
-			else
-				tagValidity="N";
-			if ((splited[1].equals("DATE")) && (splited[0].equals("1")))
-			{
-				tagValidity="N";
-			}
-			if ((splited[1].equals("NAME")) && (splited[0].equals("2")))
-			{
-				tagValidity="N";
-			}
-			//To be replaced with a collection of individual and family class objects using set methods
-			System.out.println("<-- "+splited[0]+"|"+splited[1]+"|"+tagValidity+"|"+(splited.length>2?splited[2]:""));
+
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-	} catch (Exception e) {
-		System.out.println(e);
 	}
-}
 
-	public Set<String> init()
-	{
+	public Set<String> init() {
 		File file = new File("resources/tags.properties");
 		Set<String> tagNames = new HashSet<String>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
-	
+
 			String st;
-			while ((st = br.readLine()) != null)
-			{
+			while ((st = br.readLine()) != null) {
 				tagNames.add(st);
 			}
 		} catch (Exception e) {
@@ -92,5 +139,4 @@ public class Parser {
 		return tagNames;
 	}
 
-	
 }
