82254ffdf9ce6cb15379a0f4cbde9cc508aa6701
Rozy Gupta <rozygupta024@gmail.com>
2018-10-06 19:09:58 -0400

    Update src/GEDCOM/AnomaliesParser.java

--- src/GEDCOM/AnomaliesParser.java
+++ src/GEDCOM/AnomaliesParser.java
@@ -11,13 +11,10 @@ public class AnomaliesParser {
 			
 			try {
 			for (Individual j :p.getIndividualList()) {
-	    	if(j.getDeath()!=null ) {
-							if(j.getBirthday().compareTo(j.getDeath())>0) {
-			    				  System.out.println("Error: " + "INDIVIDUAL: " + "US03: " +  j.getLineNumber() + ": " + 
-									j.getId() + ": " + "Died " + j.getDeath() + " before born " + j.getBirthday());
-							}		
-	    			
-	    		}
+	    	
+				if(j.getBirthday().compareTo(j.getDeath())<0) {
+			    	 System.out.println("Error: " + "INDIVIDUAL: " + "US03: " +  j.getLineNumber() + ": " + j.getId() + ": " + "Died " + j.getDeath() + " before born " + j.getBirthday());
+				}			
 	    	
 		}}
 			catch(Exception e) {
