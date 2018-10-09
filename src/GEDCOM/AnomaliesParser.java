e9de7a7c9849366d5506fea2c5cddec76fcbc86d
Rozy Gupta <rozygupta024@gmail.com>
2018-10-09 15:56:38 -0400

    I made a function for user story 3

--- src/GEDCOM/AnomaliesParser.java
+++ src/GEDCOM/AnomaliesParser.java
@@ -8,14 +8,20 @@ public class AnomaliesParser {
 	
 		public static void main(String[] args) {
 			Parser p = new Parser();
+		}
        // User Story 03
-				for (Individual j :p.getIndividualList()) {
-					if(j.getDeath()!= null) {
-						if((j.getBirthday()).compareTo(j.getDeath())>0) {
-							System.out.println("Error: " + "INDIVIDUAL: " + "US03: " +  j.getLineNumber() + ": " + j.getId() + ": " + "Died " + j.getDeath() + " before born " + j.getBirthday());
-						}			
+				
+				public boolean birthOccursBeforDeath() {
+					Parser p = new Parser();
+					for (Individual j :p.getIndividualList()) {
+						if(j.getDeath()!= null) {
+							if((j.getBirthday()).compareTo(j.getDeath())>0) {
+								System.out.println("Error: " + "INDIVIDUAL: " + "US03: " +  j.getLineNumber() + ": " + j.getId() + ": " + "Died " + j.getDeath() + " before born " + j.getBirthday());
+							}			
+						}
 					}
+					return true;
 				}
 	    	
-		}
+		
 }
