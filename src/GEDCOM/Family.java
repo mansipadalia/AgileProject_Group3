c929f35fbb72ddcf6a836b01b1074564b9bfaec0
Mansi Padalia <mansi25padalia@gmail.com>
2018-10-10 00:55:41 -0400

    refactor(Parser)
    - Added Property class to store value and lineNumber for the individual and family object
    - Added enum for Property types
    - Storing linenumber while parsing gedcom file

--- src/GEDCOM/Family.java
+++ src/GEDCOM/Family.java
@@ -1,80 +1,29 @@
 package GEDCOM;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Family {
+	private Map<PropertyType, Property> properties = new HashMap<>();
 
-    private String id;
-    private LocalDate married;
-    private LocalDate divorced;
-    private String husbandID;
-    private String husbandName;
-    private String wifeId;
-    private String wifeName;
-    private int linenumber;
-    private List<String> children = new ArrayList<String>();
-
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public LocalDate getMarried() {
-        return married;
-    }
-    public void setMarried(LocalDate married) {
-        this.married = married;
-    }
-
-    public LocalDate getDivorced() {
-        return divorced;
-    }
-    public void setDivorced(LocalDate divorced) {
-        this.divorced = divorced;
-    }
-
-    public String getHusbandID() {
-        return husbandID;
-    }
-    public void setHusbandID(String husbandID) {
-        this.husbandID = husbandID;
-    }
-
-    public String getHusbandName() {
-        return husbandName;
-    }
-    public void setHusbandName(String husbandName) {
-        this.husbandName = husbandName;
-    }
-
-    public String getWifeId() {
-        return wifeId;
-    }
-    public void setWifeId(String wifeId) {
-        this.wifeId = wifeId;
-    }
-
-    public String getWifeName() {
-        return wifeName;
-    }
-    public void setWifeName(String wifeName) {
-        this.wifeName = wifeName;
-    }
-	public List<String> getChildren() {
-		return children;
+	public Map<PropertyType, Property> getProperties() {
+		return properties;
 	}
-	 public int getLineNumber() {
-         return linenumber;
-     }
-     public void setLineNumber(int linenumber) {
-         this.linenumber = linenumber;
-     }
-	public void setChildren(List<String> children) {
-		this.children = children;
+
+	public void setProperties(Map<PropertyType, Property> properties) {
+		this.properties = properties;
 	}
-	
+
+	public void setProperty(PropertyType type, Property property) {
+		this.properties.put(type, property);	
+	}
+
+	public Property getProperty(PropertyType propertyType) {
+		if (this.properties.containsKey(propertyType)) {
+			return this.properties.get(propertyType);
+		}
+
+		return null;
+	}
+
 }
---
+++ src/GEDCOM/PropertyType.java
@@ -0,0 +1,11 @@
+package GEDCOM;
+
+public enum PropertyType {
+
+	// Individual Properties
+	id, name, gender, birthday, age, alive, death, child, spouse,
+
+	// Family Properties
+	married, divorced, husbandID, husbandName, wifeID, wifeName, linenumber, children
+
+}
