dce7b38e210000ae5de7d714edda803e138091d8
Rozy Gupta <rozygupta024@gmail.com>
2018-10-23 17:21:25 -0400

    Edited user story25

--- src/GEDCOM/Test_Uniqueness.java
+++ src/GEDCOM/Test_Uniqueness.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.Test;
 
 import GEDCOM.Error.ErrorType;
@@ -104,71 +103,25 @@ public class Test_Uniqueness {
 //US25
 
 @Test
-public void testuniqueFirstName_WithoutchildrenError() {
-	List<Record> IList = new ArrayList<Record>();
-	List<Record> FList = new ArrayList<Record>();
-
-	Record family = new Record();
-	family.setProperty(PropertyType.id, new Property("F05", 1));
-	family.setProperty(PropertyType.husbandID, new Property("I01", 2));
-	family.setProperty(PropertyType.husbandName, new Property("Steffi Johnson", 3));
-	family.setProperty(PropertyType.wifeID, new Property("I02", 4));
-	family.setProperty(PropertyType.wifeName, new Property("Steffi Smith", 5));
-	FList.add(family);
-
-	Parser p = new Parser(IList, FList);
-	List<Error> errors = US_Uniqueness.uniqueFirstName(p);
-
-	Error error = new Error();
-	error.setErrorType(ErrorType.ERROR);
-	error.setRecordType(RecordType.FAMILY);
-	error.setUserStoryNumber("US25");
-	error.setLineNumber(2);
-	error.setId("F05");
-	error.setMessage("Husband's First Name Steffi is same as Wife's First Name Steffi");
-	assertEquals(error.toString(), errors.get(0).toString());
-}
-@Test
-public void testuniqueFirstName_WithoutchildrenErrorSuccess() {
-	List<Record> IList = new ArrayList<Record>();
-	List<Record> FList = new ArrayList<Record>();
-
-	Record family = new Record();
-	family.setProperty(PropertyType.id, new Property("F05", 1));
-	family.setProperty(PropertyType.husbandID, new Property("I01", 2));
-	family.setProperty(PropertyType.husbandName, new Property("Johnson Smith", 3));
-	family.setProperty(PropertyType.wifeID, new Property("I02", 4));
-	family.setProperty(PropertyType.wifeName, new Property("Steffi Smith", 5));
-	FList.add(family);
-
-	Parser p = new Parser(IList, FList);
-	List<Error> errors = US_Uniqueness.uniqueFirstName(p);
-
-	Error error = new Error();
-	error.setErrorType(ErrorType.ERROR);
-	error.setRecordType(RecordType.FAMILY);
-	error.setUserStoryNumber("US25");
-	error.setLineNumber(2);
-	error.setId("F05");
-	assertEquals(0, errors.size());
-}
-
-public void testuniqueFirstName_withChildrenError_01() {
+public void testuniqueFirstNameBirthDate_error(){
+	
 	List<Record> IList = new ArrayList<Record>();
 	List<Record> FList = new ArrayList<Record>();
 
 	Record family = new Record();
 	family.setProperty(PropertyType.id, new Property("F02", 1));
-	
-	family.setProperty(PropertyType.husbandName, new Property("Sophia Johnson", 2));
 	FList.add(family);
 	Record individual = new Record();
-	individual.setProperty(PropertyType.name, new Property("Sophia Smith", 4));
+	individual.setProperty(PropertyType.name, new Property("Sophia /Smith/", 2));
+	individual.setProperty(PropertyType.birthday, new Property(LocalDate.of(1992, 02, 20), 3));
 	IList.add(individual);
-	
+	individual = new Record();
+	individual.setProperty(PropertyType.name, new Property("Sophia /Johnson/", 4));
+	individual.setProperty(PropertyType.birthday, new Property(LocalDate.of(1992, 02, 20), 5));
+	IList.add(individual);
 
 	Parser p = new Parser(IList, FList);
-	List<Error> errors = US_Uniqueness.uniqueFirstName(p);
+	List<Error> errors = US_Uniqueness.uniqueFirstNameBirthDate(p);
 
 	Error error = new Error();
 	error.setErrorType(ErrorType.ERROR);
@@ -176,144 +129,7 @@ public void testuniqueFirstName_withChildrenError_01() {
 	error.setUserStoryNumber("US25");
 	error.setLineNumber(1);
 	error.setId("F02");
-	error.setMessage("More than one individual in a family have the same name Sophia");
+	error.setMessage("More than one child in a family have the same first name Sophia and birthday 1992-02-20");
 	assertEquals(error.toString(), errors.get(0).toString());
-}
-
-public void testuniqueFirstName_withChildrenSuccess_01() {
-	List<Record> IList = new ArrayList<Record>();
-	List<Record> FList = new ArrayList<Record>();
-
-	Record family = new Record();
-	family.setProperty(PropertyType.id, new Property("F02", 1));
-	
-	family.setProperty(PropertyType.husbandName, new Property("Smith Johnson", 2));
-	FList.add(family);
-	Record individual = new Record();
-	individual.setProperty(PropertyType.name, new Property("Sophia Smith", 4));
-	IList.add(individual);
-	
-
-	Parser p = new Parser(IList, FList);
-	List<Error> errors = US_Uniqueness.uniqueFirstName(p);
-
-	Error error = new Error();
-	error.setErrorType(ErrorType.ERROR);
-	error.setRecordType(RecordType.FAMILY);
-	error.setUserStoryNumber("US25");
-	error.setLineNumber(1);
-	error.setId("F02");
-	assertEquals(0, errors.size());
-	
-}
-
-public void testuniqueFirstName_withChildrenError_02() {
-	List<Record> IList = new ArrayList<Record>();
-	List<Record> FList = new ArrayList<Record>();
-
-	Record family = new Record();
-	family.setProperty(PropertyType.id, new Property("F02", 1));
-	
-	family.setProperty(PropertyType.wifeName, new Property("Sophia Johnson", 2));
-	FList.add(family);
-	Record individual = new Record();
-	individual.setProperty(PropertyType.name, new Property("Sophia Smith", 4));
-	IList.add(individual);
-	
-
-	Parser p = new Parser(IList, FList);
-	List<Error> errors = US_Uniqueness.uniqueFirstName(p);
-
-	Error error = new Error();
-	error.setErrorType(ErrorType.ERROR);
-	error.setRecordType(RecordType.FAMILY);
-	error.setUserStoryNumber("US25");
-	error.setLineNumber(1);
-	error.setId("F02");
-	error.setMessage("More than one individual in a family have the same name Sophia");
-	assertEquals(error.toString(), errors.get(0).toString());
-}
-
-public void testuniqueFirstName_withChildrenSuccess_02() {
-	List<Record> IList = new ArrayList<Record>();
-	List<Record> FList = new ArrayList<Record>();
-
-	Record family = new Record();
-	family.setProperty(PropertyType.id, new Property("F02", 1));
-	
-	family.setProperty(PropertyType.wifeName, new Property("Smith Johnson", 2));
-	FList.add(family);
-	Record individual = new Record();
-	individual.setProperty(PropertyType.name, new Property("Sophia Smith", 4));
-	IList.add(individual);
-	
-
-	Parser p = new Parser(IList, FList);
-	List<Error> errors = US_Uniqueness.uniqueFirstName(p);
-
-	Error error = new Error();
-	error.setErrorType(ErrorType.ERROR);
-	error.setRecordType(RecordType.FAMILY);
-	error.setUserStoryNumber("US25");
-	error.setLineNumber(1);
-	error.setId("F02");
-	assertEquals(0, errors.size());
-}
-
-public void testuniqueFirstName_withChildrenError_03() {
-	List<Record> IList = new ArrayList<Record>();
-	List<Record> FList = new ArrayList<Record>();
-
-	Record family = new Record();
-	family.setProperty(PropertyType.id, new Property("F07", 1));
-	FList.add(family);
-	Record individual = new Record();
-	individual.setProperty(PropertyType.name, new Property("Alex Smith", 2));
-	IList.add(individual);
-	Record individual1 = new Record();
-	individual1.setProperty(PropertyType.name, new Property("Alex Smith", 3));
-	IList.add(individual1);
-	
-
-	Parser p = new Parser(IList, FList);
-	List<Error> errors = US_Uniqueness.uniqueFirstName(p);
-
-	Error error = new Error();
-	error.setErrorType(ErrorType.ERROR);
-	error.setRecordType(RecordType.FAMILY);
-	error.setUserStoryNumber("US25");
-	error.setLineNumber(1);
-	error.setId("F07");
-	error.setMessage("More than one individual in a family have the same name Alex");
-	assertEquals(error.toString(), errors.get(0).toString());
-}
-
-public void testuniqueFirstName_withChildrenErrorSuccess_03() {
-	List<Record> IList = new ArrayList<Record>();
-	List<Record> FList = new ArrayList<Record>();
-
-	Record family = new Record();
-	family.setProperty(PropertyType.id, new Property("F07", 1));
-	FList.add(family);
-	Record individual = new Record();
-	individual.setProperty(PropertyType.name, new Property("Smith Alex", 2));
-	IList.add(individual);
-	Record individual1 = new Record();
-	individual1.setProperty(PropertyType.name, new Property("Alex Smith", 3));
-	IList.add(individual1);
-	
-
-	Parser p = new Parser(IList, FList);
-	List<Error> errors = US_Uniqueness.uniqueFirstName(p);
-
-	Error error = new Error();
-	error.setErrorType(ErrorType.ERROR);
-	error.setRecordType(RecordType.FAMILY);
-	error.setUserStoryNumber("US25");
-	error.setLineNumber(1);
-	error.setId("F07");
-	assertEquals(0, errors.size());
-	
-}
-
+	}
 }	
