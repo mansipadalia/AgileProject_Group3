5b1229357bd8a7dfde780e6f45a63de3bd2177fb
TulsiPatel611 <43015392+TulsiPatel611@users.noreply.github.com>
2018-10-24 16:29:31 -0400

    Impletemented User stories 10 and 12
    I  have impletemented User stories 10 and 12 for sprint 2. Also, I have refactored testcases for user stories 4 and 5.

--- src/GEDCOM/US_MarriageDivorceDates.java
+++ src/GEDCOM/US_MarriageDivorceDates.java
@@ -1,6 +1,7 @@
 package GEDCOM;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -96,6 +97,10 @@ public class US_MarriageDivorceDates {
 			String wifeID = i.getProperty(PropertyType.wifeID) != null
 					? (String) i.getProperty(PropertyType.wifeID).getValue()
 					: null;
+					
+			LocalDate marriageDate = i.getProperty(PropertyType.married) != null
+					? (LocalDate) i.getProperty(PropertyType.married).getValue()
+					: null;
 
 			String[] IDs = new String[] { husbandID, wifeID };
 
@@ -109,18 +114,17 @@ public class US_MarriageDivorceDates {
 							? (LocalDate) res.get(0).getProperty(PropertyType.death).getValue()
 							: null;
 
-					if (DeathDate != null
-							&& ((LocalDate) i.getProperty(PropertyType.married).getValue()).isAfter(DeathDate)) {
+					if (DeathDate != null && marriageDate != null
+							&& (marriageDate).isAfter(DeathDate)) {
 
 						error = new Error();
 						error.setErrorType(ErrorType.ERROR);
-						error.setRecordType(RecordType.INDIVIDUAL);
+						error.setRecordType(RecordType.FAMILY);
 						error.setUserStoryNumber("US05");
 						error.setLineNumber(i.getProperty(PropertyType.married).getLineNumber());
-						error.setId(id);
-						error.setMessage("Marriage Date " + (LocalDate) i.getProperty(PropertyType.married).getValue()
-								+ " (" + i.getProperty(PropertyType.id).getValue() + ") occurs after Death Date "
-								+ DeathDate);
+						error.setId((String) i.getProperty(PropertyType.id).getValue());
+						error.setMessage("Marriage Date " + marriageDate + " (" + i.getProperty(PropertyType.id).getValue() 
+								+ ") occurs after Death Date " + DeathDate);
 						errors.add(error);
 					}
 				}
@@ -129,5 +133,55 @@ public class US_MarriageDivorceDates {
 		}
 		return errors;
 	}
+	
+	// US 10
+	public static List<Error> marriageAfter14(Parser p) {
 
+			List<Error> errors = new ArrayList<Error>();
+			Error error = new Error();
+
+			for (Record i : p.getFamilyList()) {
+				String husbandID = i.getProperty(PropertyType.husbandID) != null
+						? (String) i.getProperty(PropertyType.husbandID).getValue()
+						: null;
+				String wifeID = i.getProperty(PropertyType.wifeID) != null
+						? (String) i.getProperty(PropertyType.wifeID).getValue()
+						: null;
+						
+				LocalDate marriageDate = i.getProperty(PropertyType.married) != null
+						? (LocalDate) i.getProperty(PropertyType.married).getValue()
+						: null;
+
+				String[] IDs = new String[] { husbandID, wifeID };
+
+				for (String id : IDs) {
+
+					Predicate<Record> byId = x -> x.getProperty(PropertyType.id).getValue().equals(id);
+					List<Record> res = p.getIndividualList().stream().filter(byId).collect(Collectors.<Record>toList());
+
+					if (id != null) {
+						LocalDate birthDate = res.get(0).getProperty(PropertyType.birthday) != null
+								? (LocalDate) res.get(0).getProperty(PropertyType.birthday).getValue()
+								: null;
+
+						//if (birthDate != null && marriageDate != null && (Period.between(birthDate, marriageDate).getYears()) <= 14) {
+						if (birthDate != null && marriageDate != null && (birthDate.isBefore(marriageDate)) 
+							&& (Period.between(birthDate, marriageDate).getYears()) <= 14) {
+						
+							error = new Error();
+							error.setErrorType(ErrorType.ERROR);
+							error.setRecordType(RecordType.FAMILY);
+							error.setUserStoryNumber("US10");
+							error.setLineNumber(i.getProperty(PropertyType.married).getLineNumber());
+							error.setId((String) i.getProperty(PropertyType.id).getValue());
+							error.setMessage("Marriage " + (LocalDate) i.getProperty(PropertyType.married).getValue()
+									+ " (" + i.getProperty(PropertyType.id).getValue() + ") occurs before spouse (Birth Date: " + birthDate + ") is 14 years old.");
+							errors.add(error);
+						}
+					}
+				}
+
+			}
+			return errors;
+		}
 }
