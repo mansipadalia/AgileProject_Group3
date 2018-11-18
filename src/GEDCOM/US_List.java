668a274321ccfca39a84a0b8c12884a5dab7c82c
Sanchita Rastogi <sanchitarastogi03@gmail.com>
2018-11-18 07:27:24 -0500

    User stories 35 and 36

--- src/GEDCOM/US_List.java
+++ src/GEDCOM/US_List.java
@@ -250,4 +250,47 @@ public class US_List {
 		}
 		return records;
 	}
+	
+	// US35
+	public static List<Record> listRecentBirths(Parser p) {
+		List<Record> individuals = new ArrayList<Record>();
+		
+		for (Record i : p.getIndividualList()) {
+		
+			LocalDate birthDate = i.getProperty(PropertyType.birthday) != null
+					? (LocalDate) i.getProperty(PropertyType.birthday).getValue()
+					: null;
+					
+					if (birthDate != null && birthDate.isBefore(LocalDate.now())) {
+						LocalDate birthDay = LocalDate.of(LocalDate.now().getYear(), birthDate.getMonthValue(),
+								birthDate.getDayOfMonth());
+						long diffDays = ChronoUnit.DAYS.between(LocalDate.now(), birthDay);
+						if (diffDays < 30 && diffDays > 0) {
+										individuals.add(i);
+									}
+								}
+			}
+		return individuals;
+	}
+	
+	// US36
+		public static List<Record> listRecentDeaths(Parser p) {
+			List<Record> individuals = new ArrayList<Record>();
+			
+			for (Record i : p.getIndividualList()) {
+			
+				LocalDate deathDate = i.getProperty(PropertyType.death) != null
+						? (LocalDate) i.getProperty(PropertyType.death).getValue()
+						: null;
+			if (deathDate != null && deathDate.isBefore(LocalDate.now())) {
+				LocalDate deathDay = LocalDate.of(LocalDate.now().getYear(), deathDate.getMonthValue(),
+				deathDate.getDayOfMonth());
+				long diffDays = ChronoUnit.DAYS.between(LocalDate.now(), deathDay);
+				if (diffDays < 30 && diffDays > 0) {
+								individuals.add(i);
+							}
+						}
+				}
+			return individuals;
+		}
 }
